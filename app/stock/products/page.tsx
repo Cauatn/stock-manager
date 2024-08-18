2fe7440aea5322094443649cc100a24ef3867962
cauatn <tavarescauac@gmail.com>
2024-08-18 12:49:05 -0300

    style: better responsivity from lg screens

--- app/stock/products/page.tsx
+++ app/stock/products/page.tsx
@@ -7,7 +7,7 @@ export default function Products() {
   return (
     <div className="w-full">
       {/* <Navbar title="asd" /> */}
-      <div className="z-10 w-full max-w-7xl justify-between text-sm lg:flex flex-col gap-10 py-10 px-10 ">
+      <div className="z-10 w-full max-w-7xl justify-between text-sm lg:flex flex-col gap-10 py-10 px-10 mx-auto">
         <h1 className="text-4xl">Controle de Estoque</h1>
         <DataTable />
       </div>
